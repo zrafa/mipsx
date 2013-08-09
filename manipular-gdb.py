07c8d8965eb2287f5244e88cb9960302661aff98
rafa <rafa@selab>
2013-08-09 15:09:24 -0300

    manipular

--- manipular-gdb.py
+++ manipular-gdb.py
@@ -1,26 +1,25 @@
-
 import time
 import sys
 from subprocess import Popen, PIPE, STDOUT
 
 def salida():
-        a = p.stdout.readline()
-        while not "(gdb)" in a:
-                sys.stdout.write(a)
-                # print a
-                a = p.stdout.readline()
-
+	a = p.stdout.readline()
+	while not "(gdb)" in a:
+		sys.stdout.write(a)
+		# print a
+		a = p.stdout.readline()
+	
 
 def registros():
-        p.stdin.write('info register\n')
-        salida()
+	p.stdin.write('info register\n')
+	salida()
 
 def listado():
-        p.stdin.write('list 0\n')
-        salida()
+	p.stdin.write('list 0\n')
+	salida()
 
 
-p = Popen(['/home/rolo/opt/crossdev/toolchain-mips_r2_gcc-4.6-linaro_uClibc-0.9.33.2/bin/mips-openwrt-lin=PIPE, stdin=PIPE, stderr=STDOUT)
+p = Popen(['/home/rolo/opt/crossdev/toolchain-mips_r2_gcc-4.6-linaro_uClibc-0.9.33.2/bin/mips-openwrt-linux-gdb', 'add'], stdout=PIPE, stdin=PIPE, stderr=STDOUT)
 
 p.stdin.write('target remote 10.0.15.166:4567\n')
 salida()
@@ -40,4 +39,3 @@ p.stdin.write('run\n')
 salida()
 
 time.sleep(2)
-
