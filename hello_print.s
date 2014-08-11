eda54074fe244407860aad1779fde74303e70ef2
Rafael Ignacio Zurita <rafaelignacio.zurita@gmail.com>
2014-08-11 20:22:35 -0300

    agregamos un ejemplo que muestra un texto

---
+++ hello_print.s
@@ -0,0 +1,35 @@
+
+# File: hello.s -- Say Hello to MIPS Assembly Language Programmer
+# Author: falcon <wuzhangjin@gmail.com>, 2009/01/17
+#
+# Compilar con :
+#
+#	mips-linux-gnu-as -g --gstabs hello_print.s -o hello_print.o
+#	mips-linux-gnu-ld -static -o hello_print.elf hello_print.o 
+
+
+    .text
+    .globl main
+
+main:
+
+                      # print sth. via sys_write
+    li $a0, 1         # print to standard ouput 
+    la $a1, stradr    # set the string address
+    lw $a2, strlen    # set the string length
+    li $v0, 4004      # index of sys_write: 
+                      # __NR_write in /usr/include/asm/unistd.h
+    syscall           # causes a system call trap.
+
+                      # exit via sys_exit
+    move $a0, $0      # exit status as 0
+    li $v0, 4001      # index of sys_exit
+                      # __NR_exit in /usr/include/asm/unistd.h
+    syscall
+
+    .rdata
+stradr: .asciiz "hello, world!\n"
+strlen: .word . - stradr  # current address - the string address
+# end
+
+
