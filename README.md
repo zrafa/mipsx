874487f73d746337cec11c6d595e145579b1b0b7
Rafael Ignacio Zurita <rafaelignacio.zurita@gmail.com>
2014-08-12 15:53:54 -0300

    cambios

--- README.md
+++ README.md
@@ -4,5 +4,9 @@ mipsx
 Se necesita tener instalado
 sshpass
 python-tk
+gdb-multiarch
 
 Se necesita que en la maquina destino el usuario root tenga como clave root
+
+Memory layout
+http://www.dirac.org/linux/gdb/02a-Memory_Layout_And_The_Stack.php
--- compilarycargar.sh
+++ compilarycargar.sh
@@ -3,6 +3,7 @@
 RUTA=/usr/bin/
 AS=mips-linux-gnu-as
 LD=mips-linux-gnu-ld
+OBJDUMP=mips-linux-gnu-objdump
 IP_MIPS=10.0.15.50
 
 exec > /tmp/archivotemp.txt
@@ -11,6 +12,11 @@ exec 2>&1
 ${RUTA}/${AS} -g --gstabs $1 -o ${1}.o  &&
 
 ${RUTA}/${LD} -static -o ${1}.elf ${1}.o &&
+
+echo "---------------------------------------------"
+echo " ** DIRECCIONES EN MEMORIA ** "
+${RUTA}/${OBJDUMP} -s ${1}.elf | grep -A 1 section
+echo "---------------------------------------------"
 
 sshpass -p "root" scp ${1}.elf root@${IP_MIPS}:/tmp &&
 
