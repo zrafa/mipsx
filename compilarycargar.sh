89c09abfc79fdae1eef9922b8772e3e8ca09a47b
Rafael Ignacio Zurita <rafaelignacio.zurita@gmail.com>
2014-08-27 20:01:01 -0300

    volvemos a utilizar as y ld

--- compilarycargar.sh
+++ compilarycargar.sh
@@ -20,8 +20,8 @@ sshpass -p "root" scp ${1} root@${IP_MIPS}:/tmp &&
 ARCHIVO=`basename ${1}` 
 echo archivo nuevo : $ARCHIVO
 
-sshpass -p "root" ssh -o StrictHostKeyChecking=no root@${IP_MIPS} "cd /tmp/ && ${CC} -g ${ARCHIVO} -o ${ARCHIVO}.elf " &&
 #sshpass -p "root" ssh -o StrictHostKeyChecking=no root@${IP_MIPS} "cd /tmp/ && ${CC} -g ${ARCHIVO} -o ${ARCHIVO}.elf " &&
+sshpass -p "root" ssh -o StrictHostKeyChecking=no root@${IP_MIPS} "cd /tmp/ && ${AS} -g --gstabs ${ARCHIVO} -o ${ARCHIVO}.o && ${LD} ${ARCHIVO}.o -o ${ARCHIVO}.elf " &&
 
 sshpass -p "root" scp root@${IP_MIPS}:/tmp/${ARCHIVO}.elf /tmp/ &&
 
