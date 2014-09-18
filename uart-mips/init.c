ab6f736cbdeeb49eb8b65d9a5d41b7cb8c23dbfb
Rafael Ignacio Zurita <rafaelignacio.zurita@gmail.com>
2014-09-17 22:52:08 -0300

    leo y escribo bien

--- uart-mips/init.c
+++ uart-mips/init.c
@@ -19,6 +19,7 @@
 #include <sys/mman.h>
 
 #define UART_BASE		0x10030000
+#define PDFUNS_REGISTER		0x10010344
 
 
 #define PAGE_SIZE 4096
@@ -40,6 +41,7 @@ void uartmips_init(void) {
 		perror("/dev/mem");
 		exit(1);
 	}
+
 	uart_reg_base = mmap(NULL, PAGE_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd,
 	    UART_BASE);
 	if (uart_reg_base == MAP_FAILED) {
@@ -47,6 +49,7 @@ void uartmips_init(void) {
 		exit(1);
 	}
 
+	printf("UART Line Status Register = %0x\n", (*(volatile uint32_t *) (uart_reg_base+(20))));
 	/* mem = 3; */
 
 }
@@ -54,4 +57,15 @@ void uartmips_init(void) {
 void uartmips_exit(void) {
 
 	printf("mem=%i\n", uart_reg_base);
+	exit(0);
+}
+
+void uartmips_printf(void) {
+
+//	printf("UART Interrupt Enable Register = %0x\n", (*(volatile uint32_t *) (uart_reg_base+(4))));
+	uint32_t valor = (*(volatile uint32_t *) (uart_reg_base+(8)));
+	printf("UART FIFO Control Register = %0x\n", valor );
+//	printf("UART Line Status Register = %0x\n", (*(volatile uint32_t *) (uart_reg_base+(20))));
+//	printf("UART Line Control Register = %0x\n", (*(volatile uint32_t *) (uart_reg_base+(12))));
+	return;
 }
--- uart-mips/uartmips.s
+++ uart-mips/uartmips.s
@@ -16,6 +16,8 @@
 		.data
 uart_reg_base:	.word 0
 
+cadena:		.asciiz "Hola mundo"
+
 
 		.text
 		.global main
@@ -26,16 +28,48 @@ main:
 	la $t0, uart_reg_base
 	lw $t0, 0($t0)
 
+bucle:
+	jal esperaentrada
+	lw $t3, 0($t0)
+
+	jal imprimir_caracter
+
+	# jal uartmips_exit
+	j bucle
+	
+imprimir_cadena:
+	lb $t3, ($t4)
+	add $t6, $ra, 0
+	jal imprimir_caracter
+	add $ra, $t6, 0
+	addi $t4, $t4, 1
+	bne $t3, $zero, imprimir_cadena
+	
+
+	jal uartmips_exit
+
+# Imprimir caracter
+imprimir_caracter:
+	add $t7, $ra, 0
+	jal listo
+	add $ra, $t7, 0
+	sw $t3, 0($t0)
+	jr $ra
+
+
 # Esperamos a que el serial nos indique que podemos escribir
 listo:
 	lw $t1, 0x14($t0)	# reg_base + 0x14 es registro status en jz4740
-	andi $t1, $t1, 0x40	# 0x40 es el bit de TEMP
-	beq $t1, $zero, listo
+	andi $t2, $t1, 0x40	# 0x40 es el bit de TEMP
+	beq $t2, $zero, listo
+	jr $ra
 
-	li $t3, 0x4c		# 0x4c es la 'L' en ascii
-	sw $t3, 0($t0)
-
-	jal uartmips_exit
+# Esperamos a que el serial nos indique que podemos escribir
+esperaentrada:
+	lw $t1, 0x14($t0)	# reg_base + 0x14 es registro status en jz4740
+	andi $t2, $t1, 0x1	# 0x01 es el bit de TEMP
+	beq $t2, $zero, esperaentrada
+	jr $ra
 
 # retorna al SO
         li      $4, 88
